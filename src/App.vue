6736d3876c28505d4963db5e3142a88b5002d1d5
Kacper Borowiec <kborowiec@griddynamics.com>
2024-08-23 20:31:19 +0200

    test: added tests for dropdown component

--- src/App.vue
+++ src/App.vue
@@ -23,15 +23,19 @@ const makeAlert = (message: string) => alert(message)
         </div>
         <div class="wrapper__item">
             <h2>Dropdown</h2>
+            <!-- I built it like I would do in React
+            Probably a better option here would be to render dropdown 
+            based on arrays with object containing information about next items,
+            very great exercise though, learnt a lot -->
             <Dropdown>
                 <template #toggler>
                     <button>Open Dropdown</button>
                 </template>
                 <template #content="{ close }">
                     <DropdownContent :close="close">
-                        <DropdownItem @action="makeAlert" :child-number="1">Some action</DropdownItem>
-                        <DropdownItem @action="makeAlert" :child-number="2">Some action</DropdownItem>
-                        <DropdownItem @action="makeAlert" :child-number="3">Some action</DropdownItem>
+                        <DropdownItem @action="makeAlert" :child-number="1">Some action 1</DropdownItem>
+                        <DropdownItem @action="makeAlert" :child-number="2">Some action 2</DropdownItem>
+                        <DropdownItem @action="makeAlert" :child-number="3">Some action 3</DropdownItem>
                     </DropdownContent>
                 </template>
             </Dropdown>
